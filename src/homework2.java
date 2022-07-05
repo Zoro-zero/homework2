a81c9ba2497d16f3e20c6915e17ea352e0bafbb5
zoro <nurtilekg11@gmail.com>
2022-07-05 18:36:32 +0300

    доработка

--- src/homework2.java
+++ src/homework2.java
@@ -5,26 +5,29 @@ public class homework2 {
         home(15,43);
         home(10,99);
         home(5,73);
-        int a=0,b=100;
-        int ageRandom=a+(int)(Math.random()*b);
-        System.out.println("Ваш возраст :"+ageRandom);
-    }
-    public static void home(int age,int temperature){
-        System.out.println("Ваш возраст:"+age);
-        System.out.println("Ваша температура:"+temperature);
-        if(20<age && age==45 || -20<temperature && temperature<30) {
-            System.out.println("Можно идти гулять");
-        }
-         else if(20>age||0<temperature&&temperature<28){
-            System.out.println("Можно идти гулять");
-        }else if(age>45){
-            System.out.println("Можно идти гулять");
-        }else if (-10==temperature&& 25==temperature){
-            System.out.println();
-        }else{
-            System.out.println("Оставайтесь дома");
-        }
-
-        }
+        home(generateRandomAge(),23);
+        home(generateRandomAge(),10);
     }
 
+    public static String home (int age , int temperature){
+
+        String home1 = "Можно идти гулять";
+        String home2 = ("Оставайтесь дома");
+
+        if (age >= 20 && age <= 45 && temperature >= -20 && temperature <= 35){
+            return home1;
+        }else if (age < 20 && temperature >= 0 && temperature <= 28 ){
+            return home1;
+        }else if (age > 45 && temperature >= -10 && temperature <= 25){
+            return home1;
+        }else
+            return home2;
+
+
+
+    }
+    public static int generateRandomAge(){
+        int ageRandom=0+(int)(Math.random()*100);
+        return ageRandom;
+    }
+}
